f218e7fc60a1643b0b23ea48df588a805b0e920f
Ingmar-van-Busschbach <30147@ma-web.nl>
2019-11-29 12:40:33 +0100

    updated 02_circle
    changed JS

--- 02_cirkel/script.js
+++ 02_cirkel/script.js
@@ -4,6 +4,11 @@ const canvas = document.getElementById('canvas');
 // leg een 2d-context over de canvas
 const context = canvas.getContext('2d');
 
+var attempts = 0;
+var mathPi = Math.PI;
+var erasing = false;
+var startAngle = 1.5 * mathPi;
+var endAngle = 1.5 * mathPi;
 //maak de canvas schermvullend
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -12,21 +17,67 @@ canvas.height = height;
 
 // hier kun je op de canvas tekenen
 spawnCircle();
-function spawnCircle() {
-    setTimeout(function(){
-        var whiteColors = ["#999999", "#888888", "#777777", "#666666", "#555555", "#444444", "#333333", "#222222" ,"#111111", "#000000", "#aaaaaa", "#bbbbbb", "#cccccc", "#dddddd", "#eeeeee", "#ffffff"];
-        var rdx = Math.floor(Math.random() * 1536);
-        var rdy = Math.floor(Math.random() * 754);
-        var rdc = Math.floor(Math.random() * 16);
-        var rda = Math.floor(Math.random() * 50);
-        var rdb = Math.floor(Math.random() * 3.14);
+let circle = {};
+let circles = [];
+var whiteColors = ["#999999", "#888888", "#777777", "#666666", "#555555", "#444444", "#333333", "#222222", "#111111", "#000000", "#aaaaaa", "#bbbbbb", "#cccccc", "#dddddd", "#eeeeee", "#ffffff"];
+var rdx = Math.floor(Math.random() * 1536);
+var rdy = Math.floor(Math.random() * 754);
+var rdc = Math.floor(Math.random() * 16);
+var rda = Math.floor(Math.random() * 50);
+var rdb = Math.floor(Math.random() * 3.14);
+var rdd = Math.floor(Math.random() * 2);
+var rde = Math.floor(Math.random() * 2);
+var color = whiteColors[rdc];
 
-        context.beginPath()
-        context.lineWidth = rda;
-        context.fillStyle = whiteColors[rdc];
-        context.arc(rdx, rdy, 20, rdb, Math.PI*2-rdb);
-        context.stroke();
-        context.fill();
-        spawnCircle();
-    }, 100);
+function spawnCircle() {
+  setTimeout(function() {
+
+    if (attempts == 200) {
+      whiteColors = ["#999999", "#888888", "#777777", "#666666", "#555555", "#444444", "#333333", "#222222", "#111111", "#000000", "#aaaaaa", "#bbbbbb", "#cccccc", "#dddddd", "#eeeeee", "#ffffff"];
+      circle.rdx = Math.floor(Math.random() * 1136 + 200);
+      circle.rdy = Math.floor(Math.random() * 554 + 100);
+      circle.rdc = Math.floor(Math.random() * 16);
+      circle.rda = Math.floor(Math.random() * 35) + 50;
+      circle.rdb = Math.floor(Math.random() * 3.14);
+      circle.rdd = Math.floor(Math.random() * 10);
+      circle.rde = Math.floor(Math.random() * 10);
+      circle.color = whiteColors[rdc];
+      erasing = false;
+      circle.startAngle = 1.5 * mathPi;
+      circle.endAngle = 1.5 * mathPi;
+      attempts = 0;
+      circles.push(circle);
+    }
+
+
+    if (erasing) {
+      circle.startAngle = circle.startAngle + 0.01 * mathPi;
+      circle.rda = circle.rda * 1.005;
+      circle.rdx = circle.rdx + circle.rdd/5;
+      circle.rdy = circle.rdy + circle.rde/5;
+      circle.color = whiteColors[Math.floor(Math.random() * 3)+circle.rdc-3];
+    } else {
+      circle.endAngle = circle.endAngle + 0.01 * mathPi;
+      circle.rda = circle.rda / 1.005;
+      circle.rdx = circle.rdx + circle.rdd/5;
+      circle.rdy = circle.rdy + circle.rde/5;
+      circle.color = whiteColors[Math.floor(Math.random() * 4)+circle.rdc-3];
+    }
+
+    if (endAngle > (1.5 * mathPi)) {
+      erasing = true;
+    }
+    if (startAngle > (1.5 * mathPi)) {
+      erasing = false;
+    }
+
+    context.beginPath()
+    context.lineWidth = circle.rdb;
+    context.fillStyle = circle.color;
+    context.arc(circle.rdx, circle.rdy, circle.rda, circle.startAngle, circle.endAngle, false);
+    context.stroke();
+    context.fill();
+    attempts = attempts + 1;
+    spawnCircle();
+  }, 1);
 }
